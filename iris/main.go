2766203ebd313d12c5b8daffc1770e8239b6bcdb
pickboard <109771426+pickboard@users.noreply.github.com>
2021-11-29 22:30:21 +0000

    Update windows.md

--- iris/main.go
+++ iris/main.go
@@ -19,4 +19,4 @@ func main() {
 	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
 
 	fmt.Println("Performing Train-Test Split")
-	trainData, testData := base.InstancesTrainTestSplit(r
+	trainData, testData := base.InstancesTrainTestSplit(ra

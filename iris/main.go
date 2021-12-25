e5127c3e21f45751abfc20fc54c8f56dd58dc119
pickboard <109771426+pickboard@users.noreply.github.com>
2021-12-25 08:16:27 +0000

    chore: release 0.1.0

--- iris/main.go
+++ iris/main.go
@@ -22,4 +22,4 @@ func main() {
 	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
 	cls.Fir(trainData)
 
-	fmt.Println("Calculating the Euclidea
+	fmt.Println("Calculating the Euclidean

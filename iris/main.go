e27af975a32a026392dc5b26f6aa3781ce56c7e0
pickboard <109771426+pickboard@users.noreply.github.com>
2021-12-29 16:02:46 +0000

    updating gemfile

--- iris/main.go
+++ iris/main.go
@@ -22,4 +22,4 @@ func main() {
 	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
 	cls.Fir(trainData)
 
-	fmt.Println("Calculating the Euclidean Distance and Retu
+	fmt.Println("Calculating the Euclidean Distance and Retur

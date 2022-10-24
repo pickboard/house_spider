88ef241b3affb88f5728d02cd899f507d0429031
pickboard <109771426+pickboard@users.noreply.github.com>
2022-10-24 17:43:04 +0000

    um: Fix __swp_type()

--- iris/main.go
+++ iris/main.go
@@ -32,4 +32,4 @@ func main() {
 
 	fmt.Println("Printing Metrics Summary")
 
-	confusionMatrix, err := evaluation.GetConfusionMatrix(tes
+	confusionMatrix, err := evaluation.GetConfusionMatrix(testD

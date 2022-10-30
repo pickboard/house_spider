74c4022b53622aee1102802b66bac15545f5f00c
pickboard <109771426+pickboard@users.noreply.github.com>
2022-10-30 13:05:22 +0000

    Update phpcs.xml

--- iris/main.go
+++ iris/main.go
@@ -32,4 +32,4 @@ func main() {
 
 	fmt.Println("Printing Metrics Summary")
 
-	confusionMatrix, err := evaluation.GetConfusionMatrix(testData, prediction
+	confusionMatrix, err := evaluation.GetConfusionMatrix(testData, predictions)

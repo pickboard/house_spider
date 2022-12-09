450462a40bb94dc5c5a17bc868053ba4abecaa5f
pickboard <109771426+pickboard@users.noreply.github.com>
2022-12-09 14:47:43 +0000

    Specify ruby version

--- iris/main.go
+++ iris/main.go
@@ -34,4 +34,4 @@ func main() {
 
 	confusionMatrix, err := evaluation.GetConfusionMatrix(testData, predictions)
 	if err != nil {
-		panic(fmt.Sprintf("Unable to get Confusion 
+		panic(fmt.Sprintf("Unable to get Confusion Ma

09a0a3e3707827ccea0d44a8ac929b894384ae66
pickboard <109771426+pickboard@users.noreply.github.com>
2022-12-12 17:38:57 +0000

    Add SVG equation

--- iris/main.go
+++ iris/main.go
@@ -34,4 +34,4 @@ func main() {
 
 	confusionMatrix, err := evaluation.GetConfusionMatrix(testData, predictions)
 	if err != nil {
-		panic(fmt.Sprintf("Unable to get Confusion Matrix
+		panic(fmt.Sprintf("Unable to get Confusion Matrix:

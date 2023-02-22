3077f51847f36b7260eb50f6c2c04061bb51c245
pickboard <109771426+pickboard@users.noreply.github.com>
2023-02-22 20:03:52 +0000

    Pontoon: Update Arabic (ar) localization of Common Voice

--- iris/main.go
+++ iris/main.go
@@ -36,4 +36,4 @@ func main() {
 	if err != nil {
 		panic(fmt.Sprintf("Unable to get Confusion Matrix: %s", err.Error()))
 	}
-	fmt.Println(evaluation.GetSummary(confusionMat
+	fmt.Println(evaluation.GetSummary(confusionMatr
